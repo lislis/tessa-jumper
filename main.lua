d5d7d4c8531a32f8c554fbdbc71118521b71bdd3
lislis <mail@lislis.de>
2017-11-24 13:00:08 +0100

    left right borders

--- main.lua
+++ main.lua
@@ -1,5 +1,30 @@
-function love.load()
+function playerUpdateX()
+   player.x = player.x + (jumpForce * jumpForceDirection)
 
+   if player.x < 0 then
+      player.x = 0
+   elseif player.x > stage.width - (player.w * 0.8)  then
+      player.x = stage.width - (player.w * 0.8)
+   end
+
+end
+
+function playerUpdateY()
+   if player.isJumping then
+      player.y = player.y - jumpSpeed
+      if player.y < jumpHeight then
+         player.isJumping = false
+      end
+   else
+      player.y = player.y + (jumpSpeed * 0.4)
+      if player.y > stage.height then
+         game.state = 'over'
+      end
+   end
+end
+
+
+function love.load()
    game = {}
    game.state = 'play'
 
@@ -18,21 +43,13 @@ function love.load()
 
    jumpHeight = 150
    jumpSpeed = 3
+   jumpForceDirection = 0
+   jumpForce = 2
 end
 
-
 function love.update()
-   if player.isJumping then
-      player.y = player.y - jumpSpeed
-      if player.y < jumpHeight then
-         player.isJumping = false
-      end
-   else
-      player.y = player.y + (jumpSpeed * 0.4)
-      if player.y > stage.height then
-         game.state = 'over'
-      end
-   end
+   playerUpdateX()
+   playerUpdateY()
 end
 
 
@@ -42,4 +59,12 @@ function love.draw()
    end
 
    love.graphics.draw( player.sprite, player.x, player.y, 0, 0.2)
+end
+
+function love.keypressed( key, scancode, isrepeat )
+   if key == 'right' then
+      jumpForceDirection = 1
+   elseif key == 'left' then
+      jumpForceDirection = -1
+   end
 end
